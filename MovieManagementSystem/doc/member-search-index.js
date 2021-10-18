ba98ca8f72200adee4b6fa9990ff6ca2c01e9a71
Akshat Sawraj <39132018+AkshatSawraj@users.noreply.github.com>
2021-10-17 22:32:41 -0600

    Delete MovieManagementSystem/doc directory

--- MovieManagementSystem/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"sait.mms.application","c":"appDriver","l":"appDriver()","u":"%3Cinit%3E()"},{"p":"sait.mms.application","c":"appDriver","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- MovieManagementSystem/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"sait.mms.application","l":"appDriver"}];updateSearchResults();
